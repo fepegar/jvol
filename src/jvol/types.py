e71b9c21e0ea632bca06644c31f60ca3588c0964
Fernando Pérez-García <fepegar@gmail.com>
2024-09-04 20:09:04 +0100

    Add types module

---
+++ src/jvol/types.py
@@ -0,0 +1,14 @@
+from typing import TypeVar
+
+import numpy as np
+import numpy.typing as npt
+from numpy import generic
+
+DType = TypeVar("DType", bound=generic)
+block_shape_dtype = np.uint16
+TypeBlockIndices = npt.NDArray[np.uint8]
+TypeShapeBlockNumpy = npt.NDArray[block_shape_dtype]
+TypeRleValues = npt.NDArray[np.int32]
+TypeRleCounts = npt.NDArray[np.uint32]
+TypeShapeArray = npt.NDArray[np.uint16]
+TypeShapeTuple = tuple[int, int, int]
