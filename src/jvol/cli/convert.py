df2bb61538c701dec38f98b15a6ccc65d65a81e3
Fernando Pérez-García <fepegar@gmail.com>
2024-09-04 20:56:30 +0100

    Fix ruff config

--- src/jvol/cli/convert.py
+++ src/jvol/cli/convert.py
@@ -2,13 +2,14 @@ from __future__ import annotations  # for mkdocstrings
 
 import sys
 from pathlib import Path
+from typing import Annotated
 
 import typer
 from humanize import naturalsize
 from loguru import logger
-from typing import Annotated
 
-from jvol.io import open_image, save_image
+from jvol.io import open_image
+from jvol.io import save_image
 
 app = typer.Typer(pretty_exceptions_show_locals=False)
 
