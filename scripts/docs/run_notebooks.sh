4a9cf167c35173b6d5fde642433f93b2141b1862
Fernando Pérez-García <fepegar@gmail.com>
2024-09-29 00:19:07 +0100

    Try fixing CI

--- scripts/docs/run_notebooks.sh
+++ scripts/docs/run_notebooks.sh
@@ -1,13 +1,14 @@
 set -e
 
+# source .venv/bin/activate
 papermill_workdir="papermill_temp"
-mkdir $papermill_workdir
+mkdir -p $papermill_workdir
 cd $papermill_workdir
 
 for notebook in $(ls ../docs/*.ipynb)
 do
     echo $notebook
-    uv run papermill $notebook $notebook
+    papermill $notebook $notebook -k python3
 done
 
 cd ..
